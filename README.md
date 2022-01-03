a02bc9e09cb9ebf7dbd1350df6b124b1a26e7297
shane <shane@ms>
2022-01-03 09:28:09 +0000

    Initial draft

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # spacecar
+
+## Installation
+```bash
+git clone
+sudo apt install ruby
+sudo gem install bundler
+sudo bundle install
+```
+
+### Other dependencies
+https://github.com/filedrive-team/go-graphsplit
+
+### Usage
+spacecar.config.yaml
+```yaml
+# Number of threads to process at the same time.
+num_threads: 4
+project_name: example
+input_folder: /mnt/data/example/dataset
+# Output folder to put txt/cid/commp/car/zst files
+output_folder: /mnt/data/example/output
+# Temporary folder. Use tmpfs to get the best speed. Consumes up to 2 x sector_size.
+temp_folder: /mnt/data/example/temp
+# Default deal size for 32GB deal
+max_size_gb: 16.5
+min_size_gb: 16
+# Not used
+keep_input_files: true
+# Used to speed up graph split. Use tmpfs to get the best speed. Consumes exactly 2 x sector_size.
+tmp_dir: /mnt/a65/blockchain/temp
+# 0 to disable compression. Otherwise, generate compressed car file using zstd.
+compress_level: 12
+# Also copy car file to below mounted path
+copy:
+# Stop copying car files to mounted path if the disk available space is less than set value
+disk_available_gb: 200
+```
---
+++ spacecar.rb
@@ -0,0 +1,135 @@
+#!/usr/bin/ruby
+require 'yaml'
+require 'fileutils'
+require 'pathname'
+require 'json'
+require 'parallel'
+require 'sys/filesystem'
+config = YAML.load(File.read('spacecar.config.yaml'))
+@num_threads = config['num_threads']
+@input= config['input_folder']
+@output= config['output_folder']
+@temp= config['temp_folder']
+@max= (config['max_size_gb'] * 1024 * 1024 * 1024).to_i
+@min= (config['min_size_gb'] * 1024 * 1024 * 1024).to_i
+@keep = config['keep_input_files']
+@name = config['project_name']
+@tmp_dir = config['tmp_dir']
+@compress = config['compress_level']
+@copy = config['copy']
+@disk_available = config['disk_available_gb']
+
+unless File.directory? @input
+  raise "input folder '#{@input}' does not exist or is not a directory"
+end
+if @copy
+  @copy.each do |dst|
+    if Sys::Filesystem.mounts.none?{|mount| mount.mount_point == dst}
+      raise "copy destination #{dst} is not mounted path"
+    end
+  end
+end
+
+FileUtils.mkdir_p(@output)
+
+def generate_groups(paths)
+  groups = []
+  group = []
+  size = 0
+  paths.each do |path|
+    next unless path.file?
+    remaining = path.size
+    start = 0
+    index = 0
+    if size + path.size > @max
+      loop do
+        group << [path, start, @max - size, index]
+        groups << group
+        group = []
+        remaining -= @max - size
+        start += @max - size
+        size = 0
+        index += 1
+        break if remaining <= @max
+      end
+    end
+    if remaining + size < @min
+      group << [path, start, remaining, index] 
+      size += remaining
+    elsif remaining + size <= @max
+      group << [path, start, remaining, index]
+      groups << group
+      group = []
+      size = 0
+    end
+  end
+  groups
+end
+
+puts "== Processing the source folder"
+paths = Pathname.glob(File.join(@input, '**', '*'))
+groups = generate_groups(paths)
+
+puts "== Generating Cars with #{@num_threads} threads"
+Parallel.each_with_index(groups, in_threads: @num_threads) do |group, index|
+  puts "== [#{index}] Transfering data to temp folder"
+  temp = File.join(@temp, index.to_s)
+  temp_ipfs = File.absolute_path(File.join(@temp, index.to_s, 'ipfs'))
+  temp_dataset = File.join(@temp, index.to_s, 'dataset')
+  FileUtils.mkdir_p(temp_ipfs)
+  FileUtils.mkdir_p(temp_dataset)
+  group.each do |path, offset, length, index|
+    relative = path.relative_path_from(@input)
+    target = Pathname.new(temp_dataset).join(relative)
+    FileUtils.mkdir_p(target.dirname.to_s)
+    target = target.to_s
+    if offset != 0 || length != path.size
+      target = "%s.%04d" % [target, index]
+    end
+    IO.copy_stream(path.to_s, target, length, offset)
+  end
+  puts "== [#{index}] Adding temp folder to IPFS"
+  system("IPFS_PATH=#{temp_ipfs} ipfs init")
+  system("IPFS_PATH=#{temp_ipfs} ipfs config --json Experimental.FilestoreEnabled true")
+  result = `IPFS_PATH=#{temp_ipfs} ipfs add -r -p=false --pin=false --nocopy #{temp_dataset}`
+  cid = result.lines[-1].split(' ', 3)[1]
+
+  temp_car = "#{@temp}/#{@name}.#{index}.car"
+  cid_file = "#{@output}/#{@name}.#{index}.cid"
+  txt_file = "#{@output}/#{@name}.#{index}.txt"
+  commp_file = "#{@output}/#{@name}.#{index}.commp"
+  zst_file = "#{@output}/#{@name}.#{index}.car.zst"
+  File.write(txt_file, result)
+  File.write(cid_file, cid)
+  puts "== [#{index}] Generating car file"
+  system("IPFS_PATH=#{temp_ipfs} ipfs dag export #{cid} > #{temp_car}")
+  puts "== [#{index}] Distributing car file"
+  threads = []
+  threads.push(Thread.new{FileUtils.cp(temp_car, @output)})
+  threads.push(Thread.new{system("TMPDIR=#{File.absolute_path(@tmp_dir)} graphsplit commP #{temp_car} > #{commp_file}")})
+  if @compress > 0
+    threads.push(Thread.new{system("zstdmt -#{@compress} #{temp_car} -o #{zst_file}")})
+  end
+  if @copy
+    @copy.each do |dst|
+      threads.push(Threads.new do
+        loop do
+          if File.writable?(dst) && Sys::Filesystem.mounts.any{|mount| mount.mount_point == dst}
+            dst_stat = Sys::Filesystem.stat(dst)
+            if dst_stat.block_size * dst_stat.blocks_available / 1024 / 1024 / 1024 >= @disk_available
+              break
+            end
+          end
+          puts "Please replace disk #{dst} and make sure it's writable and has at least #{disk_available} GB space"
+          sleep 600
+        end
+        FileUtils.cp(temp_car, dst)
+      end)
+    end
+  end
+  threads.push(Thread.new{
+    FileUtils.rm_rf(File.join(@temp, index.to_s))
+  })
+  threads.each{|thread| thread.join}
+  FileUtils.rm(temp_car)
+end
